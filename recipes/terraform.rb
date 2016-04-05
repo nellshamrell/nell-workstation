1c773222140f8cab02d480c569ea565313ff4d66
Nell Shamrell <nellshamrell@gmail.com>
2016-04-05 10:22:41 -0700

    download latest version of terraform

--- recipes/terraform.rb
+++ recipes/terraform.rb
@@ -5,9 +5,9 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 execute 'download terraform' do
-  command 'wget https://releases.hashicorp.com/terraform/0.6.12/terraform_0.6.12_linux_amd64.zip'
+  command 'wget https://releases.hashicorp.com/terraform/0.6.14/terraform_0.6.14_linux_amd64.zip'
 end
 
 execute 'install terraform' do
-  command 'unzip terraform_0.6.12_linux_amd64.zip -d /bin'
+  command 'unzip terraform_0.6.14_linux_amd64.zip -d /bin'
 end
